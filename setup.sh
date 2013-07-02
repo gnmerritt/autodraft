225c5d38c5c207fe0cb1d00193f6e20ac64e20e6
Nathan Merritt <gnm@the-merritts.net>
2013-07-02 19:11:06 -0400

    added stub django project, fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
-# Sets up venv & installs necessary stuff
+# prereq: sudo easy_install virtualenv
 
-virtualenv venv/
+# Sets up venv & installs necessary stuff
+virtualenv --no-site-packages venv/
 
 source venv/bin/activate
 
-pip install sqlalchemy
+easy_install ipython django
+
+echo "All set, now to get started do:"
+echo "  source venv/bin/activate"
