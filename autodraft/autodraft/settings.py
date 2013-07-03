259df82ad281c123e7b77e2a075a16539dd8c1fd
Nathan Merritt <gnm@the-merritts.net>
2013-07-02 22:28:58 -0400

    fixed typos in model, renamed to draftHost

--- autodraft/autodraft/settings.py
+++ autodraft/autodraft/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'draftHost',
 )
 
 # A sample logging configuration. The only tangible logging
--- autodraft/draftHost/models.py
+++ autodraft/draftHost/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 import datetime
 
-class NflTeam(models.model):
+class NflTeam(models.Model):
     """NFL Team"""
     name = models.TextField()
 
-class Position(models.model):
+class Position(models.Model):
     """Football position e.g. RB or QB"""
     desc = models.TextField()
     abbreviation = models.TextField(max_length=4)
 
-class NflPlayer(models.model):
+class NflPlayer(models.Model):
     """Draft-eligible NFL player"""
     first_name = models.TextField()
     last_name = models.TextField()
@@ -18,20 +18,20 @@ class NflPlayer(models.model):
     team = models.ForeignKey(NflTeam)
     position = models.ForeignKey(Position)
 
-class FantasyDraft(models.model):
+class FantasyDraft(models.Model):
     name = models.TextField(max_length=20)
     admin = models.EmailField()
     draft_start = models.DateTimeField()
     time_per_pick = models.PositiveIntegerField()
     team_limit = models.PositiveIntegerField()
 
-class FantasyTeam(models.model):
+class FantasyTeam(models.Model):
     draft = models.ForeignKey(FantasyDraft)
     name = models.TextField(max_length=80)
     email = models.EmailField()
     auth_key = models.TextField(max_length=40) # len(`uuidgen`) == 36
 
-class FantasyUpcomingPick(models.model):
+class FantasyUpcomingPick(models.Model):
     """An upcoming pick"""
     fantasy_team = models.ForeignKey(FantasyTeam)
     expires = models.DateTimeField('expires at')
@@ -40,9 +40,9 @@ class FantasyUpcomingPick(models.model):
     class Meta:
         ordering = ('pick_number',)
 
-class FantasyPick(models.model):
+class FantasyPick(models.Model):
     """A pick that's been made"""
     fantasy_team = models.ForeignKey(FantasyTeam)
-    draft_pick = models.FantasyUpcomingPick()
-    player = models.ForeignKey(NflPlayer)
     when = models.DateTimeField(default=datetime.datetime.now())
+    draft_pick = models.ForeignKey(FantasyUpcomingPick)
+    player = models.ForeignKey(NflPlayer)
