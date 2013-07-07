b40922045288428a59e045cdecfead7d3b36cc12
Nathan Merritt <gnm@the-merritts.net>
2013-07-07 11:22:25 -0400

    updated setup script

--- setup.sh
+++ setup.sh
@@ -11,3 +11,8 @@ easy_install ipython django South simplejson readline
 
 echo "All set, now to get started do:"
 echo "  source venv/bin/activate"
+echo ""
+echo "some commands to run:"
+echo "./manage.py syncdb (builds your DB)"
+echo "./manage.py import_teams (adds Nfl Teams to DB)"
+echo "./manage.py import_positions (adds Nfl Positions to DB)"
