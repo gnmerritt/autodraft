f10db0e0b502c496dd62f5f84e18d3143fff5dc7
Nathan Merritt <gnm@the-merritts.net>
2013-07-07 22:44:27 -0400

    first pass generic json object finished

--- autodraft/draftHost/logic/objects.py
+++ autodraft/draftHost/logic/objects.py
@@ -7,23 +7,92 @@ def obj_to_json(object):
 
 
 class JsonObject(object):
+    """Super class for all other JSON objects, provides serialization.
+    To add additional fields in a subclass, define one or more of
+    functions, fields and mappings.
+    """
+    BASE_FIELDS = ['id', 'name', 'description']
+
+    def __init__(self, db_object):
+        self.d = {}
+        self.db_object = db_object
+
+    def list_to_mapping_dict(self, list):
+        return {key: key for key in list}
+
+    def eval_functions_to_dict(self, functions):
+        function_mapping = {}
+        for key, func in functions.iteritems():
+            try:
+                func_value = func()
+                function_mapping[key] = func_value
+            except: # catch everything!
+                pass
+        return function_mapping
+
     def json_response(self):
-        return obj_to_json(self.d)
+        json_dict = self.db_to_dict({},
+                                        self.list_to_mapping_dict(self.BASE_FIELDS))
+        if hasattr(self, 'fields'):
+            # check the db object for any fields
+            json_dict = self.db_to_dict(json_dict,
+                                           self.list_to_mapping_dict(self.fields))
+        if hasattr(self, 'mappings'):
+            # check the db object for any mappings
+            json_dict = self.db_to_dict(json_dict, self.mappings)
+
+        if hasattr(self, 'functions'):
+            # evaluate all the functions, then merge the two dictionaries
+            values_from_functions = self.eval_functions_to_dict(self.functions)
+            json_dict = dict(json_dict.items() + values_from_functions.items())
+
+        return obj_to_json(json_dict)
+
+    def db_to_dict(self, json_dict, fields):
+        for field, output in fields.iteritems():
+            try:
+                value = getattr(self.db_object, field)
+                if value:
+                    json_dict[output] = value
+            except AttributeError:
+                pass
+
+        return json_dict
 
 
 class JsonNflPlayer(JsonObject):
+    fields = ['first_name', 'last_name', 'jersey_number']
+
     def __init__(self, db_nfl_player):
-        self.d = {}
-        self.d['first_name'] = db_nfl_player.first_name
-        self.d['last_name'] = db_nfl_player.last_name
-        self.d['jersey_number'] = db_nfl_player.jersey_number
-        self.d['team'] = unicode(db_nfl_player.team)
-        self.d['position'] = db_nfl_player.position.abbreviation
+        super(JsonNflPlayer, self).__init__(db_nfl_player)
+        self.functions = {
+            'team_id': self.get_team_id,
+            'team': self.get_team,
+            'position': self.get_position,
+        }
+
+    def get_team_id(self):
+        return self.db_object.team.id
+
+    def get_team(self):
+        return unicode(self.db_object.team)
+
+    def get_position(self):
+        return self.db_object.position.abbreviation
 
 
 class JsonFantasyTeam(JsonObject):
+    fields = ['name', 'email']
+
     def __init__(self, db_fantasy_team):
-        self.d = {}
-        self.d['draft'] = "TODO"
-        self.d['name'] = db_fantasy_team.name
-        self.d['email'] = db_fantasy_team.email
+        super(JsonFantasyTeam, self).__init__(db_fantasy_team)
+        self.functions = {
+            'picks': self.get_picks,
+            'draft': self.get_draft,
+        }
+
+    def get_picks(self):
+        return "TODO"
+
+    def get_draft(self):
+        return "TODO"
