32d0f88df30568d285a698c1a2a3b34b90a30c7b
Nathan Merritt <gnm@the-merritts.net>
2013-07-09 22:47:39 -0400

    cleaned up nfl json objects

--- autodraft/draftHost/logic/nfl.py
+++ autodraft/draftHost/logic/nfl.py
@@ -1,14 +1,9 @@
 from json import JsonObject
 
-class JsonNflPlayer(JsonObject):
-    fields = ['first_name', 'last_name', 'jersey_number']
 
-    def __init__(self, db_nfl_player):
-        super(JsonNflPlayer, self).__init__(db_nfl_player)
-        self.functions = {
-            'team': self.get_team,
-            'position': self.get_position,
-        }
+class JsonNflPlayer(JsonObject):
+    fields = ['first_name', 'last_name']
+    functions = ['team', 'position']
 
     def get_team(self):
         return JsonNflTeam(self.db_object.team).json_dict()
@@ -16,28 +11,21 @@ class JsonNflPlayer(JsonObject):
     def get_position(self):
         return self.db_object.position.abbreviation
 
+
 class JsonNflTeam(JsonObject):
     fields = ['city']
-
-    def __init__(self, db_nfl_team):
-        super(JsonNflTeam, self).__init__(db_nfl_team)
-        self.functions = {
-            'divison' : self.get_division,
-        }
+    functions = ['division']
 
     def get_division(self):
         return JsonNflDivision(self.db_object.divison).json_dict()
 
 
 class JsonNflDivision(JsonObject):
-    def __init__(self, db_nfl_div):
-        super(JsonNflDivision, self).__init__(db_nfl_div)
-        self.functions = {
-            'conference' : self.get_conference,
-        }
+    functions = ['conference']
 
     def get_conference(self):
         return JsonNflConference(self.db_object.conference).json_dict()
+
 
 class JsonNflConference(JsonObject):
     pass # No extra fields needed
