adfedfa146ff093c71b0fd2ce0e41fb2cdfd6951
Nathan Merritt <gnm@the-merritts.net>
2013-07-11 09:31:41 -0400

    wired most of the rest of the url handlers up

--- autodraft/draftHost/views.py
+++ autodraft/draftHost/views.py
@@ -25,6 +25,7 @@ def picks(request):
     return picks.json_response()
 
 def make_pick(request, pick_id, player_id):
+    ## TODO
     context = get_context_or_error(request)
     return HttpResponse("picks/make/{p}/player/{who}/"
                         .format(p=pick_id, who=player_id))
@@ -32,10 +33,10 @@ def make_pick(request, pick_id, player_id):
 def player(request, uid):
     db_player = get_object_or_404(models.NflPlayer, pk=uid)
     json_player = nfl.JsonNflPlayer(db_player)
-    print "got player and json object {j}".format(j=json_player.json_string())
     return json_player.json_response()
 
 def search(request, query):
+    ## TODO
     return HttpResponse("search/{q}".format(q=query))
 
 def team_info_id(request, id):
@@ -43,16 +44,17 @@ def team_info_id(request, id):
     return team_response(team)
 
 def team_info_name(request, name):
-    team = {} # TODO
+    team = get_object_or_404(models.FantasyTeam, name=name)
     return team_response(team)
 
 def current_team(request):
     context = get_context_or_error(request)
-    return HttpResponse("current team")
+    return team_response(context.team)
 
 def team_response(db_team):
     json_team = fantasy.JsonFantasyTeam(db_team)
     return json_team.json_response()
 
 def register(request):
+    ## TODO
     return HttpResponse("hello, registration form")
