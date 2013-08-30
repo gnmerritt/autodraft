0a9b4fa1389a50ed814e518a0522020bd6b03afd
Nathan Merritt <gnm@the-merritts.net>
2013-08-30 15:57:01 -0400

    added directory change magic to cron scripts

--- start_prod.sh
+++ start_prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 source venv/bin/activate
 python autodraft/manage.py runserver --settings autodraft.prod-settings 0.0.0.0:8000
