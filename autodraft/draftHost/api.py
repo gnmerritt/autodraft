eae211697a8ba227c47ac8484098fda9009e86b8
Nathan Merritt <gnm@the-merritts.net>
2013-09-02 00:55:48 -0400

    moved api to /api/v1/ namespace

---
+++ autodraft/draftHost/api.py
@@ -0,0 +1,41 @@
+from django.conf.urls import patterns, url
+from draftHost import views
+
+
+urlpatterns = patterns('draftHost.views',
+    url(r'^draft/?$', views.draft_key, name='draft'),
+    url(r'^draft/(?P<id>\d+)/?$', views.draft_id, name='draft_id'),
+    url(r'^picks/?$', views.picks, name='picks'),
+    url(r'^pick_player/(?P<player_id>\d+)/?$',
+        views.make_pick, name="make_pick"),
+
+    url(r'^player/(?P<uid>\d+)/?$', views.player, name='player'),
+    url(r'^player/(?P<uid>\d+)/status/?$',
+        views.player_status, name='player_status'),
+
+    # Search views
+    url(r'^search/name/(?P<name>\w+)/?$',
+        views.search, name='search'),
+    url(r'^search/name/(?P<name>\w+)/pos/(?P<position>\w{2})?/?$',
+        views.search, name='search_name_pos'),
+    url(r'^nfl/position/(?P<position>\w{2})/?$',
+        views.search, name='position_players'),
+
+    url(r'^team/(?P<id>\d+)/?$', views.team_id, name='team_id'),
+    url(r'^team/(?P<id>\d+)/players/?$',
+        views.fantasy_team_players, name='fantasy_team_players'),
+    url(r'^team/(?P<name>.*)/?$', 'team_info_name'),
+    url(r'^team/?$', 'current_team'),
+
+    # NFL Data methods
+    url(r'^nfl/teams/?$', views.nfl_teams, name='nfl_teams'),
+    url(r'^nfl/team/(?P<id>\d+)/players/?$', views.nfl_team_with_players, name='nfl_team_with_players'),
+    url(r'^nfl/team/(?P<id>\d+)/?$', 'nfl_team'),
+    url(r'^nfl/conferences/?$', views.nfl_conferences, name='nfl_conferences'),
+    url(r'^nfl/divisions/?$', views.nfl_divisions, name='nfl_divisions'),
+    url(r'^nfl/positions/?$', 'nfl_positions'),
+
+    # College data methods
+    url(r'^colleges/?$', views.colleges, name='colleges'),
+    url(r'^college/(?P<id>\d+)/players/?$', views.college_players, name='college_players'),
+)
