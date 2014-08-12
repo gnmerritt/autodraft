dfdcd7734f3c87230e1b7c08853eb377f25a9ddd
Nathan Merritt <gnm@the-merritts.net>
2014-08-11 21:28:00 -0400

    new network at market st\!

--- start_prod.sh
+++ start_prod.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 source venv/bin/activate
-python autodraft/manage.py run_gunicorn --settings autodraft.prod-settings -b 192.168.1.42:8000 -w 4
+python autodraft/manage.py run_gunicorn --settings autodraft.prod-settings -b 192.168.0.42:8000 -w 4
