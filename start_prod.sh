693e358ad983bea49163902572c4b7b107c30496
Nathan Merritt <gnm@the-merritts.net>
2014-10-24 21:22:26 -0400

    dynamic # of threads

--- start_prod.sh
+++ start_prod.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOST_IP=`ifconfig eth0 | awk '/t addr:/{gsub(/.*:/,"",$2);print$2}'`
+CPUS=`grep -c ^processor /proc/cpuinfo`
+JOBS=$(($CPUS + 1))
 cd $DIR
 source venv/bin/activate
-python autodraft/manage.py run_gunicorn --settings autodraft.prod-settings -b ${HOST_IP}:8000 -w 4
+python autodraft/manage.py run_gunicorn --settings autodraft.prod-settings -b ${HOST_IP}:8000 -w $JOBS
